ef713a572a6b647808920563cccbf1700d4051dc
Gentris <lecigentris@gmail.com>
2020-12-13 03:12:30 +0100

    Add get_node_height function

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ class Node:
 		self.data = data
 		self.left_node = left_node
 		self.right_node = right_node
+
+
+def get_node_height(node: Node):
+	if (node is None):
+		return -1
+
+	return max(get_node_height(node.left_node), get_node_height(node.right_node)) + 1
